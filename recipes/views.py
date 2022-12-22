a41065a47a3a7b5837604e4215c18790b7d2d274
Rodrigo de Miranda Videira <diguimirandajf@gmail.com>
2022-12-21 22:54:09 -0300

    Removendo contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home 1")
-
-def contato(request):
-    return HttpResponse("Contato 1")
-
-def sobre(request):
-    return HttpResponse("Sobre 1")
+    return render(request, 'recipes/home.html', context={
+        'name': 'Rodrigo',
+    })
